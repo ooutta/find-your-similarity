dd95b4286128579e4cd27fe80aaf7be72ec358d3
alsth712 <alsth712@naver.com>
2023-05-03 15:59:09 +0900

    [FEAT] 페이지 마크업,CSS,조건 설정

---
+++ public/condition.js
@@ -0,0 +1,32 @@
+const helloBox = document.querySelector(".helloBox");
+const findBox = document.querySelector(".findBox");
+const resultBox = document.querySelector(".resultBox");
+
+const goFindBtn = document.querySelector("#goFindBtn");
+const goResultBtn = document.querySelector(".goResultBtn");
+const replayBtn = document.querySelector("#replayBtn");
+
+const helloSection = document.querySelector(".helloSection");
+const findSection = document.querySelector(".findSection");
+const resultSection = document.querySelector(".resultSection");
+
+let toggleGoFind = false;
+
+const changeToggle = (curSection, nextSection) => {
+    toggleGoFind = !toggleGoFind;
+    curSection.classList.toggle("hide");
+    
+    if (curSection.classList.contains("hide")) {
+        curSection.style.display = "none";
+        nextSection.style.display = "flex";
+    } else if(!curSection.classList.contains("hide")){
+        curSection.style.display = "none";
+        nextSection.style.display = "flex";
+    };
+};
+
+// helloBox classList에 showBox 가 있다면
+goFindBtn.addEventListener("click", () => changeToggle(helloSection, findSection));
+goResultBtn.addEventListener("click", () => changeToggle(findSection, resultSection));
+replayBtn.addEventListener("click", () => changeToggle(resultSection, helloSection));
+
